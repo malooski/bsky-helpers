4676540d84e0caa0d0aa720c4ee7c5b5eb2df974
Malooski <malooski@protonmail.com>
2023-11-10 21:53:47 -0600

    Update build stuff.

--- src/hashtag.ts
+++ src/hashtag.ts
@@ -2,7 +2,7 @@ import { Tagged } from "type-fest";
 
 export type Hashtag = Tagged<string, "Hashtag">;
 
-const HASHTAG_REGEX = /^#([a-zA-Z0-9]+)$/g;
+const HASHTAG_REGEX = /^#([a-zA-Z0-9]+)$/;
 
 export function toHashtag(s: string): Hashtag | null {
     const match = HASHTAG_REGEX.exec(s);
